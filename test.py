fc40aabee21e8940359993cbc985c8040bf1998b
rkun2010-ux <rkun2010@gmail.com>
2026-05-19 10:21:32 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,87 +1,110 @@
 import numpy as np
-import matplotlib.pyplot as plt
-import scipy.stats as stats
-from sklearn.ensemble import RandomForestRegressor, RandomForestClassifier
-from sklearn.linear_model import LogisticRegression, Ridge
-from sklearn.preprocessing import SplineTransformer, StandardScaler
+from sklearn.linear_model import LogisticRegression, LinearRegression, Ridge
+from sklearn.preprocessing import PolynomialFeatures
 from sklearn.pipeline import make_pipeline
-from tqdm import tqdm
+from sklearn.neural_network import MLPClassifier, MLPRegressor
+from scipy import stats
+import matplotlib.pyplot as plt
 
 
-# ---------------------------------------------------------
-# 1. DGP
-# ---------------------------------------------------------
-def generate_data(n, p=2):
-    X = np.random.normal(0, 1, size=(n, p))
-    f_true_x = 0.5 * X[:, 0] + 0.5 * X[:, 1]
-    pi_true = 1 / (1 + np.exp(-f_true_x))
-    D = np.random.binomial(1, pi_true)
-    mu1_true = 1.5 + 0.8 * X[:, 0] + 0.5 * X[:, 1]
-    return X, D, pi_true, mu1_true
+def estimate_ey1(X, D, Y, ps_method='l', poly_degree=4):
+    if ps_method == 'l':
+        model = LogisticRegression(penalty=None, solver='lbfgs', max_iter=1000)
+
+    elif ps_method == 'p':
+        model = make_pipeline(
+            PolynomialFeatures(degree=poly_degree, include_bias=False),
+            LogisticRegression(penalty=None, C=1.0, solver='lbfgs', max_iter=2000)
+        )
+
+    elif ps_method == 'n':
+        model = MLPClassifier(hidden_layer_sizes=(12, 12),
+                              activation='relu',
+                              solver='adam',
+                              max_iter=1000,
+                              random_state=42)
+    else:
+        raise ValueError("Unknown method")
+    model.fit(X, D)
+    e_hat = model.predict_proba(X)[:, 1]
+    e_hat = np.clip(e_hat, 1e-3, 1 - 1e-3)
+    ipw_scores = (D * Y) / e_hat
+    ey1_ipw = np.mean(ipw_scores)
+    #############################################################################
+    if ps_method == 'l':
+        or_model = LinearRegression()
+    elif ps_method == 'p':
+        or_model = make_pipeline(
+            PolynomialFeatures(degree=poly_degree, include_bias=False),
+            Ridge(alpha=0.0)
+        )
+    elif ps_method == 'n':
+        or_model = MLPRegressor(hidden_layer_sizes=(12, 12), activation='relu',
+                                solver='adam', max_iter=1000, random_state=42)
+    mask1 = (D == 1)
+    or_model.fit(X[mask1], Y[mask1])
+    mu1_hat = or_model.predict(X)
+
+    add_score = (1 - D / e_hat) * mu1_hat * np.sqrt(len(D))
+    mean_add_score = np.mean(add_score)
+
+    e = 1 / (1 + np.exp(-(0.5 * X[:, 0] - 0.5 * X[:, 1])))
+    mu1 = 1.0 + 2 * X[:, 0] + 2 * X[:, 1] + 2
+    add_score_star = (1 - D / e_hat) * mu1 * np.sqrt(len(D))
+    mean_add_score_star = np.mean(add_score_star)
+
+    return mean_add_score_star, mean_add_score, ey1_ipw
 
 
-# ---------------------------------------------------------
-# 2. 
-# ---------------------------------------------------------
-def run_simulation(method='ml', n_simulations=200, n_samples=3000):
-    bn_h_hat_list = []
-    bn_h_star_list = []
-    for _ in tqdm(range(n_simulations)):
-        X, D, pi_true, mu1_true = generate_data(n=n_samples)
-
-        if method == 'ml':
-            clf = RandomForestClassifier(n_estimators=50, max_depth=5)
-            reg = RandomForestRegressor(n_estimators=50, max_depth=5)
-
-        elif method == 'linear':
-            clf = LogisticRegression(penalty=None, max_iter=1000)
-            reg = Ridge(alpha=1.0)
-
-        elif method == 'spline':
-            spline = SplineTransformer(n_knots=5, degree=3)
-            clf = make_pipeline(spline, LogisticRegression(penalty='l2', C=1.0, max_iter=2000))
-            reg = make_pipeline(spline, Ridge(alpha=1.0))
-
-        clf.fit(X, D)
-        pi_hat = np.clip(clf.predict_proba(X)[:, 1], 1e-3, 1 - 1e-3)
-
-        Y1_observed = mu1_true + np.random.normal(0, 0.5, size=len(mu1_true))
-        reg.fit(X[D == 1], Y1_observed[D == 1])
-        mu1_hat = reg.predict(X)
-
-        h_hat = - mu1_hat / pi_hat
-        h_star = - mu1_true / pi_true
-
-        score_part = pi_hat - D
-
-        bn_h_hat_list.append(np.mean(score_part * h_hat) * np.sqrt(len(D)))
-        bn_h_star_list.append(np.mean(score_part * h_star) * np.sqrt(len(D)))
-
-    return bn_h_hat_list, bn_h_star_list
+num_simulations = 200
+sample_size = 3000
 
 
-chosen_method = 'ml'
-res_hat, res_star = run_simulation(method=chosen_method)
+def generate_data(n):
+    X = np.random.uniform(-1, 1, (n, 2))
+    logits = 0.5 * X[:, 0] - 0.5 * X[:, 1]
+    true_probs = 1 / (1 + np.exp(-logits))
+    D = np.random.binomial(1, true_probs)
 
-def standardize(data):
-    print(np.std(data))
-    print(np.mean(data))
-    return (data - np.mean(data)) / np.std(data)
+    Y_noise = np.random.normal(0, 1, n)
+    Y = 1.0 + 2 * X[:, 0] + 2 * X[:, 1] + 2.0 * D + Y_noise
+    return X, D, Y
 
 
-z_h_hat = standardize(res_hat)
-z_h_star = standardize(res_star)
+results_ipw = []
+results_dr = []
+ses_ipw = []
+ses_dr = []
+add_score_means = []
+np.random.seed(42)
 
-fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 5), sharey=True)
+for i in range(num_simulations):
+    X, D, Y = generate_data(n=sample_size)
+    ey1_ipw, add_score, IPW = estimate_ey1(X, D, Y, ps_method='n')
 
-stats.probplot(z_h_hat, dist="norm", plot=ax1)
+    results_ipw.append(ey1_ipw)
+    add_score_means.append(add_score)
+    results_dr.append(IPW)
+
+mean_add = np.mean(add_score_means)
+emp_sd_add = np.std(add_score_means)
+
+print("=" * 60)
+print(f"{'Metric':<20} | {'IPW':<15}  ")
+print("-" * 60)
+print(f"{'True E[Y(1)]':<20} | {3.0000:<15.4f}  ")
+print(f"{'Mean Est E[Y(1)]':<20}| {np.mean(results_dr):<15.4f}| {np.mean(results_ipw):<15.4f}| {np.std(results_ipw):<15.4f} | {mean_add:<15.4f} | {emp_sd_add:<15.4f} ")
+
+fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(10, 5), sharey=True)
+
+stats.probplot((add_score_means - np.mean(add_score_means)) / np.std(add_score_means), dist="norm", plot=ax1)
 ax1.set_title("")
-ax1.set_xlabel(fr"Theoretical Quantiles ($B_n[\hat{{h}}_n]$)")
+ax1.set_xlabel(fr"Theoretical quantiles (standardized $B_n[\hat{{h}}_n]$)")
 ax1.set_ylabel(r"Sample Quantiles")
 
-stats.probplot(z_h_star, dist="norm", plot=ax2)
+stats.probplot((results_ipw - np.mean(results_ipw)) / np.std(results_ipw), dist="norm", plot=ax2)
 ax2.set_title("")
-ax2.set_xlabel(fr"Theoretical Quantiles ($B_n[h^*]$)")
+ax2.set_xlabel(fr"Theoretical quantiles (standardized $B_n[h^*]$)")
 ax2.set_ylabel("")
 
 for ax in [ax1, ax2]:
